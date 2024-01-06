121ce13f92482f668908a679bd61fc057a1bdaa8
Jed Schmidt <jschmidt@quantummetric.com>
2024-01-06 12:38:53 -0500

    meh, go ahead and push artifacts

---
+++ dist/2048.js
@@ -0,0 +1,1 @@
+let r=Uint8Array.from(atob("JAEcGgQBARovAQoBBAEFFwEfAceKBAwOBQcBAQHCgQUBAgIEAQEGAQEDAQEBFAFTAcKLCMKmASYCAQYpRxsEBC0rIwIBYwEBDwIHAgoDAgEQAQEeHVkLARghCQIEAQU="),e=>e.codePointAt()),f=Array.from(new TextDecoder().decode(r),e=>e.codePointAt());function*i(){let e=0,o=0;for(let t of f){if(o%2)for(;t--;)yield String.fromCodePoint(e++);else e+=t;o++}}export{i as default};
